0760d08c6a486ec503786aa6d4df4d372da91a3f
Matthew <mcitron@cern.ch>
2014-07-20 12:02:08 +0100

    lx06

--- jCaloTower/CaloTowerAnalyser/batch/batch_executable_filelist.sh
+++ jCaloTower/CaloTowerAnalyser/batch/batch_executable_filelist.sh
@@ -1,6 +1,7 @@
-cd /afs/cern.ch/work/m/mcitron/jadjets/CMSSW_6_2_0/src/jCaloTower/CaloTowerAnalyser/batch/
+cd /vols/cms04/mc3909/jadjets/CMSSW_6_2_0/src/jCaloTower/CaloTowerAnalyser/batch/
+source /vols/cms/grid/setup.sh
 eval `scramv1 runtime -sh` 
 #filelist='/afs/cern.ch/work/m/mcitron/jadjets/CMSSW_6_2_0/src/jCaloTower/CaloTowerAnalyser/qcd_PU140.txt'
-filelist='/afs/cern.ch/work/m/mcitron/jadjets/CMSSW_6_2_0/src/jCaloTower/CaloTowerAnalyser/skimfiles/140708_ttbar_PU40.txt'
-outputdir="/afs/cern.ch/work/m/mcitron/public/TTBAR/140710/"
+filelist='/vols/cms04/mc3909/jadjets/CMSSW_6_2_0/src/jCaloTower/CaloTowerAnalyser/qcd_PU40.txt'
+outputdir="/vols/cms04/mc3909/QCDPU40/"
 python python/run_qcd.py $filelist $outputdir
